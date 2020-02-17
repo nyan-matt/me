95a49878e4b5d2ca653962dd20125f0011f37a63
matt <matt.rea@gmail.com>
2020-02-17 10:11:03 -0600

    whitelisting tags from purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
       options: {
         develop: false, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-        whitelist: ['table', 'tr', 'th', 'td'],
+        whitelist: ['table', 'tr', 'th', 'td', 'blockquote'],
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
